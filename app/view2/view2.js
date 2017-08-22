2cbeff214c053b544242fb4183626006f9282ab5
ajiteshui <31252372+ajiteshui@users.noreply.github.com>
2017-08-22 15:22:21 -0400

    code changes for step5

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -12,6 +12,24 @@ angular.module('myApp.view2', ['ngRoute'])
 .controller('View2Ctrl', ['$scope', 'appService', function($scope, appService) {
 	function success(data){
 		$scope.data = data;
+		$scope.categories = [];
+		$scope.names = [];
+		$scope.finalObj = {};
+		angular.forEach($scope.data, function(value){
+			if($scope.categories.indexOf(value.category) == -1){
+				$scope.categories.push(value.category);
+			}
+			if($scope.names.indexOf(value.name) == -1){
+				$scope.names.push(value.name);
+			}
+			if(!$scope.finalObj[value.category]){
+				$scope.finalObj[value.category] = {};
+			}
+			if(!$scope.finalObj[value.category][value.name]){
+				$scope.finalObj[value.category][value.name] = {};
+			}
+			$scope.finalObj[value.category][value.name]	= value.amount;
+		});
 	}
 
 	function error(){
